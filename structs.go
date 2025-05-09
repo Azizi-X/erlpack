63c7283f0d684169ad0e8f5e4eff50f3b4ea6220
Azizi <gangmemberzizi@outlook.com>
2025-05-09 09:46:09 -0500

    Add files via upload

---
+++ structs.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"slices"
+	"strings"
+)
+
+type Struct struct {
+	raw     any
+	value   reflect.Value
+	TagName string
+}
+
+func NewStruct(s any) *Struct {
+	return &Struct{
+		raw:     s,
+		value:   strctVal(s),
+		TagName: "json",
+	}
+}
+
+func strctVal(s any) reflect.Value {
+	v := reflect.ValueOf(s)
+
+	for v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+
+	if v.Kind() != reflect.Struct {
+		panic("not struct")
+	}
+
+	return v
+}
+
+func (s *Struct) structFields() []reflect.StructField {
+	t := s.value.Type()
+
+	var f []reflect.StructField
+
+	for i := range t.NumField() {
+		field := t.Field(i)
+		if field.PkgPath != "" {
+			continue
+		}
+
+		if tag := field.Tag.Get(s.TagName); tag == "-" {
+			continue
+		}
+
+		f = append(f, field)
+	}
+
+	return f
+}
+
+type tagOptions []string
+
+func (t tagOptions) Has(opt string) bool {
+	return slices.Contains(t, opt)
+}
+
+func parseTag(tag string) (string, tagOptions) {
+	res := strings.Split(tag, ",")
+	return res[0], res[1:]
+}
+
+func (s *Struct) FillMap(out map[string]any) {
+	if out == nil {
+		return
+	}
+
+	fields := s.structFields()
+
+	for _, field := range fields {
+		name := field.Name
+		val := s.value.FieldByName(name)
+		isSubStruct := false
+		var finalVal any
+
+		tagName, tagOpts := parseTag(field.Tag.Get(s.TagName))
+		if tagName != "" {
+			name = tagName
+		}
+
+		if tagOpts.Has("omitempty") {
+			zero := reflect.Zero(val.Type()).Interface()
+			current := val.Interface()
+
+			if reflect.DeepEqual(current, zero) {
+				continue
+			}
+		}
+
+		if !tagOpts.Has("omitnested") {
+			finalVal = s.nested(val)
+
+			v := reflect.ValueOf(val.Interface())
+			if v.Kind() == reflect.Ptr {
+				v = v.Elem()
+			}
+
+			switch v.Kind() {
+			case reflect.Map, reflect.Struct:
+				isSubStruct = true
+			}
+		} else {
+			finalVal = val.Interface()
+		}
+
+		if tagOpts.Has("string") {
+			s, ok := val.Interface().(fmt.Stringer)
+			if ok {
+				out[name] = s.String()
+			}
+			continue
+		}
+
+		if isSubStruct && (tagOpts.Has("flatten")) {
+			for k := range finalVal.(map[string]any) {
+				out[k] = finalVal.(map[string]any)[k]
+			}
+		} else {
+			out[name] = finalVal
+		}
+	}
+}
+
+func (s *Struct) Map() map[string]any {
+	out := make(map[string]any)
+	s.FillMap(out)
+	return out
+}
+
+func (s *Struct) nested(val reflect.Value) any {
+	var finalVal any
+
+	v := reflect.ValueOf(val.Interface())
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+
+	switch v.Kind() {
+	case reflect.Struct:
+		n := NewStruct(val.Interface())
+		n.TagName = s.TagName
+		m := n.Map()
+
+		if len(m) == 0 {
+			finalVal = val.Interface()
+		} else {
+			finalVal = m
+		}
+	case reflect.Map:
+		mapElem := val.Type()
+		switch val.Type().Kind() {
+		case reflect.Ptr, reflect.Array, reflect.Map,
+			reflect.Slice, reflect.Chan:
+			mapElem = val.Type().Elem()
+			if mapElem.Kind() == reflect.Ptr {
+				mapElem = mapElem.Elem()
+			}
+		}
+
+		if mapElem.Kind() == reflect.Struct ||
+			(mapElem.Kind() == reflect.Slice &&
+				mapElem.Elem().Kind() == reflect.Struct) {
+			m := make(map[string]any, val.Len())
+			for _, k := range val.MapKeys() {
+				m[k.String()] = s.nested(val.MapIndex(k))
+			}
+			finalVal = m
+			break
+		}
+
+		finalVal = val.Interface()
+	case reflect.Slice, reflect.Array:
+		if val.Type().Kind() == reflect.Interface {
+			finalVal = val.Interface()
+			break
+		}
+
+		if val.Type().Elem().Kind() != reflect.Struct &&
+			!(val.Type().Elem().Kind() == reflect.Ptr &&
+				val.Type().Elem().Elem().Kind() == reflect.Struct) {
+			finalVal = val.Interface()
+			break
+		}
+
+		slices := make([]any, val.Len())
+		for x := range val.Len() {
+			slices[x] = s.nested(val.Index(x))
+		}
+		finalVal = slices
+	default:
+		finalVal = val.Interface()
+	}
+
+	return finalVal
+}
